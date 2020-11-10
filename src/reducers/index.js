69ecfcc641085c9fd3855043f948694fa5689249
Administrator <admin@example.com>
2020-11-10 21:40:46 +0900

    reducer追加

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,15 @@
-const initialState = {
-    todos: [
-        {id: 0, title: "JavaScript"},
-        {id: 1, title: "React"},
-        {id: 2, title: "TypeScript"},
-    ]
-};
-
-const todoReducer = (state = initialState, actioon) => {
-    switch(actioon.type) {
+const todoReducer = (state = [], action) => {
+    switch(action.type) {
+        case "ADD_TODO":
+            return [...state, { id: action.id, title: action.title, done: action.done }];
+        case "REMOVE_TODO":
+            const newState = state.filter(todo => todo.id !== action.id);
+            return newState;
+        case "TOGGLE_TODO":
+            const toggleState = [...state];
+            const todo = toggleState.find(todo => todo.id === action.id);
+            todo.done = !todo.done;
+            return toggleState;
         default:
             return state;
     }

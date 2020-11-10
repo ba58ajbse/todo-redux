3c1b9ee612375f9d8e80c572d8f340fa1e0bc4e2
shibuya <shibuya@pyxos-jk.co.jp>
2020-11-10 10:09:52 +0900

    コンポーネント読み込み

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Form />
+        <TodoList />
       </header>
     </div>
   );

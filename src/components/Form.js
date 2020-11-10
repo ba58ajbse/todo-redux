0a3983cf05c32541376d78e8309016d2c3789645
Administrator <admin@example.com>
2020-11-10 21:41:43 +0900

    form追加

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../actions/index';
 
 const Form = () => {
     const [inputVal, setInputVal] = useState('');
+    const dispatch = useDispatch();
 
-    const handleSubmit = ((e) => {
+    const handleSubmit = (e => {
         e.preventDefault();
-        console.log(inputVal);
+        dispatch(addTodo(inputVal));
         setInputVal('');
     });
-    
+
     return (
         <form onSubmit={handleSubmit}>
             <input
                 type="text"
                 value={inputVal}
-                onChange={(e) => setInputVal(e.target.value)}
+                onChange={e => setInputVal(e.target.value)}
             />
             <button type="submit">ADD</button>
         </form>

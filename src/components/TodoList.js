7444e77861e871381aa19a56f2d635baedd595dd
Administrator <admin@example.com>
2020-11-10 21:42:08 +0900

    Todolist修正

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeTodo, toggleTodo } from '../actions/index';
 
 const TodoList = () => {
-    const todos = useSelector((state) => state.todos);
-    const list = todos.map(todo => <li key={todo.id}>{todo.title}</li>)
+    const todos = useSelector(state => {
+        return state;
+    });
+    const dispatch = useDispatch();
+
+    const list = todos.map(todo => {
+        return (
+            <li
+                key={todo.id}
+                className={todo.done ? "done" : ""}
+            >
+                <button
+                    type="button"
+                    onClick={() => dispatch(toggleTodo(todo.id))}
+                >done</button>
+                {todo.title}
+                <button
+                    type="button"
+                    onClick={() => dispatch(removeTodo(todo.id))}
+                >remove</button>
+            </li>
+            )
+        })
 
     return (
         <ul>{list}</ul>

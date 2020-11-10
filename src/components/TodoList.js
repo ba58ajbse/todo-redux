2b0a04bfb091ddaf57c1ed88eac8d694874fdb47
Administrator <admin@example.com>
2020-11-10 22:47:11 +0900

    Todoコンポーネントに分割

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,34 +1,16 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { removeTodo, toggleTodo } from '../actions/index';
+import { useSelector } from 'react-redux';
+import Todo from './Todo';
 
 const TodoList = () => {
     const todos = useSelector(state => {
         return state;
     });
-    const dispatch = useDispatch();
-
-    const list = todos.map(todo => {
-        return (
-            <li
-                key={todo.id}
-                className={todo.done ? "done" : ""}
-            >
-                <button
-                    type="button"
-                    onClick={() => dispatch(toggleTodo(todo.id))}
-                >done</button>
-                {todo.title}
-                <button
-                    type="button"
-                    onClick={() => dispatch(removeTodo(todo.id))}
-                >remove</button>
-            </li>
-            )
-        })
 
     return (
-        <ul>{list}</ul>
+        <ul>
+            {todos.map(todo => <Todo key={todo.id} todo={todo} />)}
+        </ul>
     )
 }
 
